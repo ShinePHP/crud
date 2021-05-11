773d5ba32de6a000073d4e3fa4a7a919fb5cf8bd
Adam McGurk <amcgurk@bamboohr.com>
2021-05-11 12:28:12 -0600

    Boom, first method, written

--- src/ShinePHP/Crud/Utility.php
+++ src/ShinePHP/Crud/Utility.php
@@ -2,4 +2,17 @@
 
 namespace ShinePHP\Crud;
 
-class Utility {}
+class CrudUtility {
+
+	static function sanitize_name(string $raw_name, array $allow_list = []): string {
+		$searched_for_key = \array_search($raw_name, $allow_list);
+		if (!empty($allow_list) && $searched_for_key === false) {
+			throw new UnexpectedValueException('Value ' . $raw_name . ' does not exist in the value whitelist');
+		} else if (!empty($allow_list) && $searched_for_key !== false) {
+			return $allow_list[$searched_for_key];
+		} else {
+			return '`' . \str_replace('`', '``', $raw_name) . '`';
+		}
+	}
+
+}
