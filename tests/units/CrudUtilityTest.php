44186463215cf1150c9d33b92fe39047d754d8f4
Adam McGurk <amcgurk@bamboohr.com>
2021-07-15 14:01:34 -0600

    Wrote a second test to make sure escaping works

--- tests/units/CrudUtilityTest.php
+++ tests/units/CrudUtilityTest.php
@@ -7,6 +7,7 @@ final class CrudUtilityTest extends TestCase {
 
 	public function testDoesSanitizeRawString(): void {
 		$this->assertEquals(CrudUtility::sanitizeName('user_id'), '`user_id`');
+		$this->assertEquals(CrudUtility::sanitizeName('`user_id`'), '```user_id```');
 	}
 
 }
