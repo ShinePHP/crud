d5d808b198569493bfc87090efd3793aab1e9262
Adam McGurk <amcgurk@bamboohr.com>
2021-07-15 13:59:22 -0600

    camel case

--- src/ShinePHP/Crud/CrudUtility.php
+++ src/ShinePHP/Crud/CrudUtility.php
@@ -4,14 +4,14 @@ namespace ShinePHP\Crud;
 
 class CrudUtility {
 
-	static function sanitize_name(string $raw_name, array $allow_list = []): string {
-		$searched_for_key = \array_search($raw_name, $allow_list);
-		if (!empty($allow_list) && $searched_for_key === false) {
-			throw new UnexpectedValueException('Value ' . $raw_name . ' does not exist in the value whitelist');
-		} else if (!empty($allow_list) && $searched_for_key !== false) {
-			return $allow_list[$searched_for_key];
+	static function sanitizeName(string $rawName, array $allowList = []): string {
+		$searchedForKey = \array_search($rawName, $allowList);
+		if (!empty($allowList) && $searchedForKey === false) {
+			throw new UnexpectedValueException('Value ' . $rawName . ' does not exist in the value allow list');
+		} else if (!empty($allowList) && $searchedForKey !== false) {
+			return $allowList[$searchedForKey];
 		} else {
-			return '`' . \str_replace('`', '``', $raw_name) . '`';
+			return '`' . \str_replace('`', '``', $rawName) . '`';
 		}
 	}
 
