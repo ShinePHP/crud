58dd3fde4e4f41e5c72e6043ec971a123e5e206e
Adam McGurk <amcgurk@bamboohr.com>
2021-07-15 13:56:27 -0600

    Changing name

---
+++ tests/units/CrudUtilityTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use ShinePHP\Crud\CrudUtility;
+
+final class CrudUtilityTest extends TestCase {
+
+	public function testDoesSanitizeRawString(): void {
+		$this->assertTrue(true);
+	}
+
+}
